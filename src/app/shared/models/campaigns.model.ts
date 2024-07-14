6b146f040e5c79b1bc70e679f0e2ee82b0074d74
waghhhh <53828789+waghhhh@users.noreply.github.com>
2024-07-15 03:11:10 +0900

    deploy

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1,4 +1,20 @@
-export const Campaigns = [
+export const Campaigns = [ {
+  title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
+  label: '마감비아 마법대학의 마법사들', // 저장소에서 표시할 캠페인 이름
+  platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
+  npcs: [
+  ],
+  logs: [
+    {
+      index: '5', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
+      name: '0화', // 저장소에서 표시할 파일 이름
+      images: [
+      ],
+      interfaces: [
+      ],
+    },
+  ]
+},
   {
     title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
     label: '혐오체 금고', // 저장소에서 표시할 캠페인 이름
@@ -33,23 +49,6 @@ export const Campaigns = [
       {
         index: '4', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
         name: '최종화', // 저장소에서 표시할 파일 이름
-        images: [
-        ],
-        interfaces: [
-        ],
-      },
-    ]
-  },
-  {
-    title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
-    label: '마감비아 마법대학의 마법사들', // 저장소에서 표시할 캠페인 이름
-    platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
-    npcs: [
-    ],
-    logs: [
-      {
-        index: '5', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
-        name: '0화', // 저장소에서 표시할 파일 이름
         images: [
         ],
         interfaces: [
