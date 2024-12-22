6896015173066cfc918d2018d876fa9c9f65e38f
waghhhh <53828789+waghhhh@users.noreply.github.com>
2024-12-22 10:41:15 +0900

    deploy

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -110,6 +110,14 @@ export const Campaigns = [
       interfaces: [
       ],
     },
+    {
+      index: '18', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
+      name: '13화', // 저장소에서 표시할 파일 이름
+      images: [
+      ],
+      interfaces: [
+      ],
+    },
   ]
 },
   {
