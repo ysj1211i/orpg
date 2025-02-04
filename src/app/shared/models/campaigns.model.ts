38d613f316fbf879f3618298aff5bf2f8f0974a3
waghhhh <53828789+waghhhh@users.noreply.github.com>
2025-02-04 11:10:35 +0900

    deploy

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -159,10 +159,7 @@ export const Campaigns = [
       ],
     },  
   ]
-},
-]
-export const Campaigns = [ 
- {
+},{
   title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
   label: '마감비아 외전', // 저장소에서 표시할 캠페인 이름
   platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
@@ -178,10 +175,7 @@ export const Campaigns = [
       ],
     },
   ]
-},
-]
-export const Campaigns = [ 
-  {
+},{
     title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
     label: '혐오체 금고', // 저장소에서 표시할 캠페인 이름
     platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
