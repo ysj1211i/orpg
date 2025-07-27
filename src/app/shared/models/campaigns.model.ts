02b32492e65efecb9c886a822a4fd5f7e0d6b4ff
waghhhh <53828789+waghhhh@users.noreply.github.com>
2025-07-28 06:48:01 +0900

    deploy

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -1,7 +1,7 @@
 export const Campaigns = [ 
   {
   title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
-  label: '마감비아 마법대학의 마법사들', // 저장소에서 표시할 캠페인 이름
+  label: '서버샀죠', // 저장소에서 표시할 캠페인 이름
   platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
   npcs: [
   ],
@@ -328,7 +328,23 @@ export const Campaigns = [
         ],
       },
     ]
-  },
+  },{
+    title: 'TEST', // orpg\src\campaigns에서 세션 로그가 들어있는 폴더 이름
+    label: '랜서', // 저장소에서 표시할 캠페인 이름
+    platform: 'FVTT', // 롤20이면 "roll20", FVTT면 "FVTT"
+    npcs: [
+    ],
+    logs: [
+      {
+        index: '39', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
+        name: '1화', // 저장소에서 표시할 파일 이름
+        images: [
+        ],
+        interfaces: [
+        ],
+      },
+    ]
+  }
 ]
 export interface Campaign {
   title: string;
